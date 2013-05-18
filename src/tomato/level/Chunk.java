0783c97f32feb573f0eae26cdd417907304ac7d2
Dominik Schröter <domi@icereed.net>
2013-05-18 14:32:22 +0200

    Some documentary done on tomato.Placeable and tomato.level.Chunk

--- src/tomato/level/Chunk.java
+++ src/tomato/level/Chunk.java
@@ -6,37 +6,81 @@ import java.util.List;
 
 import tomato.Placeable;
 
+/**
+ * A chunk is a datastructure which allows to split a whole level up into smaller chunks in order to make the collision detection more efficient.
+ * @author Icereed
+ *
+ * @param <E> Some placeable class.
+ */
+
+
 @SuppressWarnings("serial")
-public class Chunk<E extends Placeable> {
+public class Chunk<E extends Placeable> implements Placeable {
 
 	List<E> content;
 	Rectangle bounds;
 
-	public Chunk() {
-		content = new ArrayList<E>();
-		bounds = new Rectangle();
-	}
-
+	/**
+	 * Constructs an empty chunk with the given bounds.
+	 * 
+	 * @param bounds
+	 *            The bounds of the chunk
+	 */
 	public Chunk(Rectangle bounds) {
-		this();
+		content = new ArrayList<E>();
 		this.bounds = bounds;
 	}
 
-	public boolean add(E e) {
-		if (e.getBounds().intersects(this.getBounds())) {
-			content.add(e);
+	/**
+	 * This Method adds an placable object to the chunk, but only when it is not
+	 * outside of the bounds.
+	 * 
+	 * @param placeable
+	 *            A placable Object to be added to the chunk.
+	 * @return Whether it is added to the chunk.
+	 * @see Placeable getBounds()
+	 */
+	public boolean add(E placeable) {
+		if (placeable.getBounds().intersects(this.getBounds())) {
+			content.add(placeable);
 			return true;
 		} else {
 			return false;
 		}
 	}
 
+	/**
+	 * Gets the content of the chunk as a list.
+	 * 
+	 * @return A list, which contains every object in the chunk.
+	 */
 	public List<E> getContent() {
 		return content;
 	}
 
+	@Override
 	public Rectangle getBounds() {
 		return bounds;
+	}
+
+	@Override
+	public int getX() {
+		return (int) bounds.getX();
+	}
+
+	@Override
+	public int getY() {
+		return (int) bounds.getX();
+	}
+
+	@Override
+	public int getWidth() {
+		return (int) bounds.getWidth();
+	}
+
+	@Override
+	public int getHeight() {
+		return (int) bounds.getHeight();
 	}
 
 }
