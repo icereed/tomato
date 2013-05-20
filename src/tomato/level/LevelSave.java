a6d0feeda5a1e77fb058c34d1db075ce9936f45f
Dominik Schröter <domi@icereed.net>
2013-05-21 01:32:03 +0200

    Forgot to refactor LevelSave. Now added correctly, even when not used.

--- src/tomato/level/LevelSave.java
+++ src/tomato/level/LevelSave.java
@@ -2,6 +2,7 @@ package tomato.level;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.List;
 
 import tomato.entity.AbstractEntity;
 import tomato.wall.Wall;
@@ -11,10 +12,10 @@ public class LevelSave {
 	private ArrayList<Wall> walls;
 	private Point position;
 
-	public LevelSave(ArrayList<AbstractEntity> entities, ArrayList<Wall> walls,
+	public LevelSave(ArrayList<AbstractEntity> entities, List<Wall> list,
 			Point position) {
 		this.entities = new ArrayList<AbstractEntity>(entities);
-		this.walls = new ArrayList<Wall>(walls);
+		this.walls = new ArrayList<Wall>(list);
 		this.position = position;
 	}
 
